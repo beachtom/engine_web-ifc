85da9e1a3523fe72119705b44611015bc8b9971b
Tom Beach <beachth@cf.ac.uk>
2026-09-25 08:25:03 +0100

    Fixes

--- src/cpp/web-ifc/geometry/generators/implementation/IfcLightSourceDirectional.cpp
+++ src/cpp/web-ifc/geometry/generators/implementation/IfcLightSourceDirectional.cpp
@@ -1,7 +1,7 @@
 #include <spdlog/spdlog.h>
 #include "../generators.h"
 namespace webifc::geometry::generators {
-	glm::dvec4 GenerateIfcLightSourceDirectional(const uint32_t expressID, const uint32_t lineType, webifc::parsing::IfcLoader &loader, webifc::cache::IfcCache &cache, GeometryGeneratorSettings &settings, bool cacheOff) {
+	glm::dvec4 GenerateIfcLightSourceDirectional(const uint32_t expressID, const uint32_t, webifc::parsing::IfcLoader &loader, webifc::cache::IfcCache &cache, GeometryGeneratorSettings &settings, const bool cacheOff) {
 		if (!cacheOff) {
 			auto cacheHit = cache.Get<glm::dvec4>(expressID);
 			if (cacheHit.has_value()) return cacheHit->get();
