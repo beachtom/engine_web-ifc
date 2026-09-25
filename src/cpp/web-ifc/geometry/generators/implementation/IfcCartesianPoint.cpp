d108faaf9826170c1a81e148be38e96c6247d87b
Tom Beach <beachth@cf.ac.uk>
2026-09-25 08:19:30 +0100

    Fixes

--- src/cpp/web-ifc/geometry/generators/implementation/IfcCartesianPoint.cpp
+++ src/cpp/web-ifc/geometry/generators/implementation/IfcCartesianPoint.cpp
@@ -1,9 +1,11 @@
 #include <spdlog/spdlog.h>
 #include "../generators.h"
 namespace webifc::geometry::generators {
-	glm::dvec3 GenerateIfcCartesianPoint(const uint32_t expressID, const uint32_t, webifc::parsing::IfcLoader &loader, webifc::cache::IfcCache &cache, GeometryGeneratorSettings &) {
-		auto cacheHit = cache.Get<glm::dvec3>(expressID);
-		if (cacheHit.has_value()) return cacheHit->get();
+	glm::dvec3 GenerateIfcCartesianPoint(const uint32_t expressID, const uint32_t lineType, webifc::parsing::IfcLoader &loader, webifc::cache::IfcCache &cache, GeometryGeneratorSettings &settings, bool cacheOff) {
+		if (!cacheOff) {
+			auto cacheHit = cache.Get<glm::dvec3>(expressID);
+			if (cacheHit.has_value()) return cacheHit->get();
+		}
 		spdlog::debug("[GenerateIfcCartesianPointImpl({})]", expressID);
 		loader.MoveToArgumentOffset(expressID, 0);
     	loader.GetTokenType();
@@ -11,7 +13,7 @@ namespace webifc::geometry::generators {
     	double y = loader.GetDoubleArgument();
     	double z = loader.GetOptionalDoubleParam(0);
     	glm::dvec3 result(x, y, z);
-		cache.Cache(expressID,result);
+		if (!cacheOff) cache.Cache(expressID,result);
 		return result;
 	}
 }
--- src/cpp/web-ifc/geometry/generators/implementation/IfcCartesianPointList3D.cpp
+++ src/cpp/web-ifc/geometry/generators/implementation/IfcCartesianPointList3D.cpp
@@ -1,9 +1,11 @@
 #include <spdlog/spdlog.h>
 #include "../generators.h"
 namespace webifc::geometry::generators {
-	std::vector<glm::dvec3> GenerateIfcCartesianPointList3D(const uint32_t expressID, const uint32_t, webifc::parsing::IfcLoader &loader, webifc::cache::IfcCache &cache, GeometryGeneratorSettings &) {
-		auto cacheHit = cache.Get<std::vector<glm::dvec3>>(expressID);
-		if (cacheHit.has_value()) return cacheHit->get();
+	std::vector<glm::dvec3> GenerateIfcCartesianPointList3D(const uint32_t expressID, const uint32_t lineType, webifc::parsing::IfcLoader &loader, webifc::cache::IfcCache &cache, GeometryGeneratorSettings &settings, bool cacheOff) {
+		if (!cacheOff) {
+			auto cacheHit = cache.Get<std::vector<glm::dvec3>>(expressID);
+			if (cacheHit.has_value()) return cacheHit->get();
+		}
 		spdlog::debug("[GenerateIfcCartesianPointList3DImpl({})]", expressID);
 		std::vector<glm::dvec3> result;
 		loader.MoveToArgumentOffset(expressID, 0);
@@ -16,7 +18,7 @@ namespace webifc::geometry::generators {
 		    result.emplace_back(x, y, z);
 		}
       	loader.GetTokenType();
-		cache.Cache(expressID,result);
+		if (!cacheOff) cache.Cache(expressID,result);
 		return result;
 	}
 }
